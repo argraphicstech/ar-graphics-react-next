68a232453ade4e55aeab7d1dc7a5df6ac124d481
argraphicstech <argraphicstech@gmail.com>
2026-06-01 15:24:17 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 
       <ul className="nav-links">
         <li><Link href="/">HOME</Link></li>
-        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#about">ABOUT US</a></li>
         <li><Link href="/location">LOCATION</Link></li>
         <li><Link href="/contact">CONTACT US</Link></li>
       </ul>
