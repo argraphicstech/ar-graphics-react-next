de148ed65a708aa180dd451ddfedf26ddf697ed6
argraphicstech <argraphicstech@gmail.com>
2026-07-30 13:47:40 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export const metadata = {
-  title: "AR Rohilla Graphics",
+  title: "AR Graphics",
   description: "Printing & Digital Services",
 
   verification: {
